645b691fdc9af02220112ea2db22c4bd70a2e19f
chen <23054115+cwkang1998@users.noreply.github.com>
2023-07-05 18:31:14 +0800

    add zk circuit example

--- zkcircuits/src/circuit-example.ts
+++ zkcircuits/src/circuit-example.ts
@@ -20,14 +20,64 @@ const main = async () => {
 
     methods: {
       init: {
+        privateInputs: [Field],
+        method(circuitState: CircuitState, secret: Field) {
+          circuitState.stepValue = Field(1);
+          circuitState.commitment = Poseidon.hash([secret]);
+          return circuitState.stepValue;
+        },
       },
       step: {
+        privateInputs: [Field, SelfProof],
+
+        method(
+          circuitState: CircuitState,
+          secret: Field,
+          earlierProof: SelfProof<CircuitState, Field>
+        ) {
+          earlierProof.publicInput.commitment.assertEquals(
+            Poseidon.hash([secret])
+          );
+          earlierProof.verify();
+          return earlierProof.publicInput.stepValue.add(circuitState.stepValue);
+        },
       },
     },
   });
 
   console.log("Compiling...");
   const { verificationKey } = await ZKCircuit.compile();
+
+  const firstProof = await ZKCircuit.init(
+    { commitment: Poseidon.hash([Field(156561223)]), stepValue: Field(1) },
+    Field(156561223)
+  );
+  console.log(
+    `firstProof Verification: ${await verify(firstProof, verificationKey)}`
+  );
+  console.log(`firstProof stepValue: ${firstProof.publicOutput.toString()}`);
+
+  const subsequentProof = await ZKCircuit.step(
+    { commitment: Poseidon.hash([Field(156561223)]), stepValue: Field(5) },
+    Field(156561223),
+    firstProof
+  );
+  console.log(
+    `subsequentProof Verification: ${await verify(subsequentProof, verificationKey)}`
+  );
+  console.log(`subsequentProof stepValue: ${subsequentProof.publicOutput.toString()}`);
+
+
+  // This will fail because the secret is wrong
+  const badProof = await ZKCircuit.step(
+    { commitment: Poseidon.hash([Field(156561223)]), stepValue: Field(5) },
+    Field(101010),
+    subsequentProof
+  );
+  console.log(
+    `badProof Verification: ${await verify(badProof, verificationKey)}`
+  );
+  console.log(`badProof stepValue: ${badProof.publicOutput.toString()}`);
 };
 
 main();
